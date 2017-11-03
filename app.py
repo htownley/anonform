17a1d1af07106824de2ab9915e649173c4e192c9
Henry Townley <htownley@me.com>
2017-11-03 17:11:54 -0400

    got rid of stylish folder

--- app.py
+++ app.py
@@ -81,17 +81,6 @@ def william():
 
 '''
 
-@app.route("scrap")
-def scrap():
-	page = requests.get('http://econpy.pythonanywhere.com/ex/001.html')
-	tree = html.fromstring(page.content)
-	#This will create a list of buyers:
-	buyers = tree.xpath('//div[@title="buyer-name"]/text()')
-	#This will create a list of prices
-	prices = tree.xpath('//span[@class="item-price"]/text()')
-	print 'Buyers: ', buyers
-	print 'Prices: ', prices
-'''
 
 if __name__ == "__main__":
     # Change the host and port name if you want. The default config is port 
